690e0a722b6cd7a1573937730400c732c1105487
civic-tools-jp <miha.yasu2409str@gmail.com>
2026-08-23 00:31:54 +0900

    Add files via upload

--- assets/js/state.js
+++ assets/js/state.js
@@ -1,2 +1,3 @@
 "use strict";
-window.appSession=JSON.parse(localStorage.getItem("aisapo_session")||localStorage.getItem("gdbv2_session")||"null"),records=[],contacts=[],branches=[],areas=[],users=[],map,markers={},editing=null,editingContact=null,editingPoster=null,editStatus="unvisited",currentAreaId=localStorage.getItem("aisapo_area")||"",branchMessages=[];
+window.appSession = JSON.parse(localStorage.getItem("aisapo_session") || localStorage.getItem("gdbv2_session") || "null");
+let records=[], contacts=[], branches=[], areas=[], users=[], map, markers={}, editing=null, editingContact=null, editingPoster=null, editStatus="unvisited", currentAreaId=localStorage.getItem("aisapo_area")||"", branchMessages=[];
